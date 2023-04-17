b38b1951f6877e7be5a6c6687f378e04b7f5c59a
[felixramon27] <[felixramon27@hotmail.com]>
2023-04-17 18:58:07 -0400

    documentacion del programa agregada

--- utils.c
+++ utils.c
@@ -8,17 +8,40 @@
 #include "utils.h"
 
 /* Funcion de lectura y evaluación de la entrada*/
+/**
+ * The function recursively opens directories and counts the number of regular files, while applying
+ * filters and printing file names and sizes based on flags.
+ * 
+ * @param dirroot a string representing the path of the directory to be opened and searched for files
+ * and subdirectories.
+ * @param nivel an integer representing the current level of recursion in the directory tree traversal.
+ * It starts at 0 for the initial directory and increments by 1 for each subdirectory encountered.
+ * @param contador A pointer to an integer variable that keeps track of the number of files processed
+ * by the function. The function increments this variable by 1 each time it processes a regular file.
+ * 
+ * @return The function does not return anything, as its return type is `void`.
+ */
 void abrirDirectorio(char *dirroot, int nivel, int *contador)
 {
     /* Directorio */
     DIR *directorio;
 
     /* punto a cada entrada en el directorio */
+    /* `struct dirent *entrada;` is declaring a pointer variable `entrada` of type `struct dirent`.
+    This pointer will be used to point to a `dirent` structure that contains information about a
+    directory entry (file or subdirectory) when iterating through a directory using the `readdir()`
+    function. */
     struct dirent *entrada;
 
+    /* `directorio = opendir(dirroot);` is opening the directory specified by the `dirroot` parameter
+    and returning a pointer to a `DIR` structure that contains information about the directory. This
+    pointer is stored in the `directorio` variable. */
     directorio = opendir(dirroot);
 
     /*Chequear si No se puede abrir el directorio */
+    /* This code block is checking if the directory specified by the `dirroot` parameter can be opened.
+    If the directory cannot be opened, it prints an error message "Error al abrir directorio" (Error
+    opening directory) to the console. */
     if (directorio == NULL)
     {
         printf("Error al abrir directorio.\n");
@@ -65,6 +88,10 @@ void abrirDirectorio(char *dirroot, int nivel, int *contador)
         }
     }
 
+    /* This code block is checking if the directory specified by the `dirroot` parameter can be closed
+    using the `closedir()` function. If the directory cannot be closed, it prints an error message
+    "Error al cerrar directorio" (Error closing directory) to the console. If the directory is
+    successfully closed, the function returns without doing anything else. */
     if (closedir(directorio) == -1)
     {
         printf("Error al cerrar directorio.\n");
@@ -75,6 +102,13 @@ void abrirDirectorio(char *dirroot, int nivel, int *contador)
 }
 
 /* Funcion que almacena los parametros obtenidos por los flags y verifica su correctitud */
+/**
+ * The function captures a flag from the command line arguments.
+ * 
+ * @param argv An array of strings containing the command line arguments passed to the program.
+ * @param argc `argc` is an integer that represents the number of arguments passed to the program from
+ * the command line, including the name of the program itself.
+ */
 int capturarFlag(char *argv[], int argc) {
     int i = 1;
 
@@ -138,6 +172,14 @@ int capturarFlag(char *argv[], int argc) {
 }
 
 /* Funcion que verifica si un inodo es un archivo regular */
+/**
+ * The function checks if a given path corresponds to a regular file.
+ * 
+ * @param path A string representing the path to a file.
+ * 
+ * @return The function `esArchivoRegular` returns an integer value indicating whether the file at the
+ * given path is a regular file or not. It returns 1 if the file is a regular file, and 0 otherwise.
+ */
 int esArchivoRegular(const char *path)
 {
     struct stat path_stat;
@@ -146,6 +188,14 @@ int esArchivoRegular(const char *path)
 }
 
 /* Funcion que verifica si un inodo es un directorio */
+/**
+ * The function checks if a given path is a directory.
+ * 
+ * @param path A string representing the path of the file or directory to be checked.
+ * 
+ * @return The function `esDirectorio` returns an integer value of 0 or 1, indicating whether the given
+ * path is a directory or not.
+ */
 int esDirectorio(const char *path) {
    struct stat statbuf;
    if (stat(path, &statbuf) != 0)
@@ -154,6 +204,13 @@ int esDirectorio(const char *path) {
 }
 
 /* Funcion que retorna el tamano de un archivo en KB */
+/**
+ * The function calculates the size of a file in kilobytes and returns the result.
+ * 
+ * @param filename A string containing the name of the file for which we want to determine the size.
+ * 
+ * @return the size of the file in kilobytes (KB), rounded up to the nearest integer.
+ */
 int tamanoArchivo(char *filename) {
     FILE *fichero;
     long int tamano;
